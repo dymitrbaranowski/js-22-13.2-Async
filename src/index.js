f11f05b16bf76b7b56ed922163a581c0e6e50a1b
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-11-15 11:58:26 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// import './js/r - get';
+// import './js/c - post';
+// import './js/u - patch';
+// import './js/d - delete';
+
+function add(a, b) {
+  return a + b;
+}
+
+console.log(add);
+console.log(add(2, 3));
