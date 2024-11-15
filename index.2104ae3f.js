98d3b0b13f690a6c9f19123566b5e07a7ed573bf
dymitrbaranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-11-15 09:59:38 +0000

    Deploying to gh-pages from @ dymitrbaranowski/js-22-13.2-Async@f11f05b16bf76b7b56ed922163a581c0e6e50a1b 🚀

---
+++ index.2104ae3f.js
@@ -0,0 +1,2 @@
+function o(o,n){return o+n}console.log(o),console.log(o(2,3));
+//# sourceMappingURL=index.2104ae3f.js.map
---
+++ index.2f040164.js
@@ -0,0 +1,2 @@
+!function(){function o(o,n){return o+n}console.log(o),console.log(o(2,3))}();
+//# sourceMappingURL=index.2f040164.js.map
